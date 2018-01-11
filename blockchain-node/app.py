18658424be755cf4e111ce8ddf352fc2e3d9587a
Kyle Dotterrer <kyle.dotterrer.18@dartmouth.edu>
2018-01-10 22:19:58 -0500

    cast to int

--- blockchain-node/app.py
+++ blockchain-node/app.py
@@ -175,5 +175,5 @@ def format_public_key(key):
 # SCRIPT
 
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
